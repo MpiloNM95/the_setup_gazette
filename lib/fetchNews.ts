0b4befa22de06cc10dd356fbd00553830c063135
MpiloNM95 <mpilonmafu95@gmail.com>
2023-03-25 19:52:55 +0200

    api setup

---
+++ lib/fetchNews.ts
@@ -0,0 +1,15 @@
+const fetchNews = async (
+    category?: Category | string,
+    keywords?: string,
+    isDynamic?: boolean
+) => {
+    // GraphQL query
+
+    // Fetch Function with Next.js 13 caching...
+
+    // Sort Function by images vs not images present
+
+    // return the results
+};
+
+export default fetchNews;
--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,29 @@
+
+type Article = {
+  author: string | null;
+  categroy: string;
+  country: string;
+  description: string;
+  image: string | null;
+  language: string;
+  published_at: string;
+  source: string;
+  title: string;
+  url: string;
+};
+
+type Pagination = {
+  count: Int;
+  limmit: Int;
+  offset: Int;
+  total: Int;
+};
+
+type NewsResponse = {
+  pagination: Pagination;
+  data: Article[];
+};
+
 type Category =
   | "general"
   | "business"
