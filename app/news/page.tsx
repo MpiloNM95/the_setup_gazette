a4bab136553d160a33a9f97c96c16c1ec2c599c3
MpiloNM95 <mpilonmafu95@gmail.com>
2023-03-28 18:52:24 +0200

    channging page . tsx folders around to see what works?

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,6 +1,6 @@
-import fetchNews from "../../../lib/fetchNews"
-import NewsList from "../../NewsList"
-import { categories } from "../../.././constants"
+import fetchNews from "../../lib/fetchNews"
+import NewsList from ".././NewsList"
+import { categories } from "../.././constants"
 
 type Props = {
     params: { category: Category };
